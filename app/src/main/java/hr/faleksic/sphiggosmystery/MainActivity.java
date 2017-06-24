3e0734a57da09dd258ea281095f2253b7313002b
Filip Aleksić <filipp.aleksic@gmail.com>
2017-06-24 21:06:49 +0200

    Added design for main screen, disabled crashlytics on dev
    Activity main xml is added with picture and buttons, no functionalty. Disabled crashlytics on dev so it doesn't send crash reports while developing

--- app/src/main/java/hr/faleksic/sphiggosmystery/MainActivity.java
+++ app/src/main/java/hr/faleksic/sphiggosmystery/MainActivity.java
@@ -3,6 +3,8 @@ package hr.faleksic.sphiggosmystery;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+        Fabric.with(this, crashlyticsKit);
         setContentView(R.layout.activity_main);
     }
+
+
 }
