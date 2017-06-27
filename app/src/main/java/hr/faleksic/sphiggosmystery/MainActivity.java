acd389ce76e2f623f572d1835202fa9adee02565
Filip Aleksić <filipp.aleksic@gmail.com>
2017-06-27 23:29:11 +0200

    Changed colors and start game activity on play button

--- app/src/main/java/hr/faleksic/sphiggosmystery/MainActivity.java
+++ app/src/main/java/hr/faleksic/sphiggosmystery/MainActivity.java
@@ -1,7 +1,11 @@
 package hr.faleksic.sphiggosmystery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
 
@@ -17,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         Fabric.with(this, crashlyticsKit);
         setContentView(R.layout.activity_main);
+
+        ImageButton playButton = (ImageButton) findViewById(R.id.button_main_play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), GameActivity.class));
+                finish();
+            }
+        });
     }
 
 
